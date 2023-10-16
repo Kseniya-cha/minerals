efe24db1b5fe0a55b8ca860c5a20d92c3cdefa50
Kseniya-cha <kseniyachapygova@gmail.com>
2023-10-16 08:51:28 +0300

    fix calculation of minerals to kg with correct equal

--- pkg/calculations/calcFromGiven.go
+++ pkg/calculations/calcFromGiven.go
@@ -33,40 +33,44 @@ func CalcFromGiven(info internal.Info) error {
 			if product.Id == givenProduct.Id {
 				fmt.Println(product.Animal, product.Part+":", givenProduct.Mass, "g")
 				result.Mass += givenProduct.Mass
-				result.Kkal += 100 * givenProduct.Mass / product.Kkal
-				result.Protein += product.Protein / given.Cat.Mass / 1000
-				result.Fat += product.Fat / given.Cat.Mass / 1000
-				result.Carbs += product.Carbs / given.Cat.Mass / 1000
-				result.Water += product.Water / given.Cat.Mass / 1000
+				result.Kkal += product.Kkal * givenProduct.Mass / 100
+				result.Protein += product.Protein * givenProduct.Mass / 100 / given.Cat.Mass / 1000
+				result.Fat += product.Fat * givenProduct.Mass / 100 / given.Cat.Mass / 1000
+				result.Carbs += product.Carbs * givenProduct.Mass / 100 / given.Cat.Mass / 1000
+				result.Water += product.Water * givenProduct.Mass / 100 / given.Cat.Mass / 1000
 
-				result.Ca += product.Ca / given.Cat.Mass / 1000
-				result.P += product.P / given.Cat.Mass / 1000
-				result.Mg += product.Mg / given.Cat.Mass / 1000
-				result.Na += product.Na / given.Cat.Mass / 1000
-				result.K += product.K / given.Cat.Mass / 1000
-				result.Fe += product.Fe / given.Cat.Mass / 1000
-				result.A += product.A / given.Cat.Mass / 1000
-				result.Omega3 += product.Omega3 / given.Cat.Mass / 1000
-				result.Omega6 += product.Omega6 / given.Cat.Mass / 1000
+				result.Ca += product.Ca * givenProduct.Mass / 100 / given.Cat.Mass / 1000
+				result.P += product.P * givenProduct.Mass / 100 / given.Cat.Mass / 1000
+				result.Mg += product.Mg * givenProduct.Mass / 100 / given.Cat.Mass / 1000
+				result.Na += product.Na * givenProduct.Mass / 100 / given.Cat.Mass / 1000
+				result.K += product.K * givenProduct.Mass / 100 / given.Cat.Mass / 1000
+				result.Fe += product.Fe * givenProduct.Mass / 100 / given.Cat.Mass / 1000
+				result.A += product.A * givenProduct.Mass / 100 / given.Cat.Mass / 1000
+				result.Omega3 += product.Omega3 * givenProduct.Mass / 100 / given.Cat.Mass / 1000
+				result.Omega6 += product.Omega6 * givenProduct.Mass / 100 / given.Cat.Mass / 1000
 			}
 		}
 	}
 
-	fmt.Printf("Mass: %0.1f g\n", result.Mass)
-	fmt.Printf("Kkal: %0.1f\n", result.Kkal)
-	fmt.Printf("Protein: %0.3f g/kg\n", result.Protein)
-	fmt.Printf("Fat: %0.3f g/kg\n", result.Fat)
-	fmt.Printf("Carbs: %0.3f g/kg\n", result.Carbs)
-	fmt.Printf("Water: %0.3f g/kg\n", result.Water)
-	fmt.Printf("Ca: %0.3f g/kg\n", result.Ca)
-	fmt.Printf("P: %0.3f g/kg\n", result.P)
-	fmt.Printf("Mg: %0.3f g/kg\n", result.Mg)
-	fmt.Printf("Na: %0.3f g/kg\n", result.Na)
-	fmt.Printf("K: %0.3f g/kg\n", result.K)
-	fmt.Printf("Fe: %0.3f g/kg\n", result.Fe)
-	fmt.Printf("A: %0.3f g/kg\n", result.A)
-	fmt.Printf("Omega3: %0.3f g/kg\n", result.Omega3)
-	fmt.Printf("Omega6: %0.3f g/kg\n", result.Omega6)
+	fmt.Println("")
+	fmt.Printf("Масса порции: %0.1f g\n", result.Mass)
+	fmt.Printf("Масса кошки: %0.1f кг\n", given.Cat.Mass)
+	fmt.Println("")
+	fmt.Printf("Калорийность: %0.1f ккал\n", result.Kkal)
+	fmt.Printf("Белок: %0.1f г/кг\n", result.Protein)
+	fmt.Printf("Жир: %0.1f г/кг\n", result.Fat)
+	fmt.Printf("Углеводы: %0.2f г/кг\n", result.Carbs)
+	fmt.Printf("Влага: %0.1f г/кг (%0.1f%%)\n", result.Water, result.Water/result.Mass*100)
+	fmt.Println("")
+	fmt.Printf("Ca: %0.3f г/кг\n", result.Ca)
+	fmt.Printf("P: %0.3f г/кг\n", result.P)
+	fmt.Printf("Mg: %0.3f г/кг\n", result.Mg)
+	fmt.Printf("Na: %0.3f г/кг\n", result.Na)
+	fmt.Printf("K: %0.3f г/кг\n", result.K)
+	fmt.Printf("Fe: %0.3f г/кг\n", result.Fe)
+	fmt.Printf("A: %0.3f г/кг\n", result.A)
+	fmt.Printf("Omega3: %0.3f г/кг\n", result.Omega3)
+	fmt.Printf("Omega6: %0.3f г/кг\n", result.Omega6)
 
 	return nil
 }
